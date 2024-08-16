354d6f53b9ce6f761a9b758a4337802ffbe0b73f
PEDRO CAMPANA DE SOUZA <pedro.5973@aluno.pr.senac.br>
2024-08-15 22:22:21 -0300

    Primeiro commit

---
+++ index.php
@@ -0,0 +1,81 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Curriculo Online Senac</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <header>
+        <div>
+            <h1>Pedro Campana de Souza</h1>
+            <h2>Tenho 18 anos e estou cursando ADS na Faculdade Senac Cascavel.</h2>
+        </div>
+        <div>
+            <img class="foto" src="monotast.jpg" alt="">        
+        </div>
+    </header>
+    <div class="sobre">
+        <h1>Sobre</h1>
+        <p>
+            Tenho 18 anos, estou cursando ADS no Senac Maringá, trabalho na empresa DB1 como estagiário na área de Qualidade de Processos.<br>
+            Me destaco pela minha criatividade e inovação no ambiente de trabalho, buscando sempre resolver os problemas trazendo algo novo e de fácil utilização.<br>
+            Possuo experiência com desenvolvimento web, auditorias internas e ISOs. Fiz 2 anos de robótica, onde aprendi um pouco de linguace C++, também realizei um curso básico sobre SQL online.
+        </p>
+    </div>
+    <div class="profissional">
+        <h1>Experiência profissional</h1>
+        <p>
+            Tenho 18 anos, estou cursando ADS no Senac Maringá, trabalho na empresa DB1 como estagiário na área de Qualidade de Processos.<br>
+            Me destaco pela minha criatividade e inovação no ambiente de trabalho, buscando sempre resolver os problemas trazendo algo novo e de fácil utilização.<br>
+            Possuo experiência com desenvolvimento web, auditorias internas e ISOs. Fiz 2 anos de robótica, onde aprendi um pouco de linguace C++, também realizei um curso básico sobre SQL online.
+        </p>
+    </div>
+    <div class="educacao">
+        <h1>Educação</h1>
+        <p>
+            Tenho 18 anos, estou cursando ADS no Senac Maringá, trabalho na empresa DB1 como estagiário na área de Qualidade de Processos.<br>
+            Me destaco pela minha criatividade e inovação no ambiente de trabalho, buscando sempre resolver os problemas trazendo algo novo e de fácil utilização.<br>
+            Possuo experiência com desenvolvimento web, auditorias internas e ISOs. Fiz 2 anos de robótica, onde aprendi um pouco de linguace C++, também realizei um curso básico sobre SQL online.
+        </p>
+    </div>
+    <div class="habilidades">
+        <h1>Habilidades</h1>
+        <p>
+            Tenho 18 anos, estou cursando ADS no Senac Maringá, trabalho na empresa DB1 como estagiário na área de Qualidade de Processos.<br>
+            Me destaco pela minha criatividade e inovação no ambiente de trabalho, buscando sempre resolver os problemas trazendo algo novo e de fácil utilização.<br>
+            Possuo experiência com desenvolvimento web, auditorias internas e ISOs. Fiz 2 anos de robótica, onde aprendi um pouco de linguace C++, também realizei um curso básico sobre SQL online.
+        </p>
+    </div>
+    <div class="projetos">
+        <h1>Projetos</h1>
+        <p>
+            Tenho 18 anos, estou cursando ADS no Senac Maringá, trabalho na empresa DB1 como estagiário na área de Qualidade de Processos.<br>
+            Me destaco pela minha criatividade e inovação no ambiente de trabalho, buscando sempre resolver os problemas trazendo algo novo e de fácil utilização.<br>
+            Possuo experiência com desenvolvimento web, auditorias internas e ISOs. Fiz 2 anos de robótica, onde aprendi um pouco de linguace C++, também realizei um curso básico sobre SQL online.
+        </p>
+    </div>
+    <div class="contato">
+        <h1>Contato</h1>
+        <div class="social">
+            <a href="https://www.linkedin.com/in/pedro-campana-de-souza" target="_blank"><img src="icons8-linkedin-48.png" alt="">LinkedIn</a>
+            <a href="https://www.instagram.com/pedro_cpsz" target="_blank"><img src="icons8-instagram-48.png" alt="">Instagram</a>
+            <a href="https://www.facebook.com/pedro.campanadesouza" target="_blank"><img src="icons8-facebook-48.png" alt="">Facebook</a>
+        </div>
+        <div class="formulario">
+            <h2>Formulário para contato:</h2>
+            <form>
+                <label class="nome">Nome completo:</label>
+                <input type="text" name="nome"><br>
+                <label class="email">E-mail:</label>
+                <input type="email" name="email"><br>
+                <label class="tel">Telefone:</label>
+                <input type="tel" name="telefone"><br>
+                <input class="enviar" type="submit" value="ENVIAR">
+            </form>
+        </div>
+    </div>
+</body>
+
+</html>
