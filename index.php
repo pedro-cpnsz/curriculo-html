4e80882d011e84370701791f387269db9c3dcd53
PEDRO CAMPANA DE SOUZA <pedro.5973@aluno.pr.senac.br>
2024-08-23 22:17:40 -0300

    CSS 50% pronto

--- index.php
+++ index.php
@@ -17,28 +17,36 @@
         </div>
     </header>
     <div class="sobre">
+        <hr class="solid">
         <h1>Sobre</h1>
+        <hr class="solid">
         <p>
             Durante toda minha infância eu estive ao lado dos meus pais acompanhando o trabalho deles, meu pai, sempre trabalhou na área de infraestrutura do TI, principalmente com o sistema Proteus da TOTVS, minha mãe, trabalhou em diferentes software houses e em diferentes cargos, suporte, sucesso do cliente, testes e implantação.<br>
             Com isso eu aprendi a gostar de tecnologia e me interessei rapidamente pela informática, sempre interessado em entender como esses sistemas que eles sustentavam, foram criados.
         </p>
     </div>
     <div class="profissional">
+        <hr class="solid">
         <h1>Experiência profissional</h1>
+        <hr class="solid">
         <p>
             Tive algumas experiências ainda antes de estudar a fundo o mundo da tecnologia, trabalhei alguns meses em uma assistência técnica para eletrônicos e posteriormente montando, formatando e otimizando computadores.<br>
             Minha primeira experiência profissional foi em 2022, em uma software house em Maringá chamada DB1 Group, eu atuava em um produto chamado Anymarket. Entrei como jovem aprendiz na área de controle de qualidade processual, realizando auditorias e análises em cima dos processos que envolviam o produto, desde o desenvolvimento até o pós-venda.<br>
-            Saí da DB1 após quase 2 anos atuando na empresa, hoje atuo em projetos na empresa Lasse de Toledo, meu tio é sócio desta empresa de materiais elétricos e juntos desenvolvemos e construimos protótipos para diferentes fins.
+            Saí da DB1 após 2 anos atuando na empresa, hoje atuo em projetos na empresa Lasse de Toledo, meu tio é sócio desta empresa de materiais elétricos e juntos desenvolvemos e construimos protótipos para diferentes fins.
         </p>
     </div>
     <div class="educacao">
+        <hr class="solid">
         <h1>Educação</h1>
+        <hr class="solid">
         <p>
             Hoje estou cursando Análise e Desenvolvimento de Sistemas na Faculdade Senac Cascavel, no momento faltam apenas 2 semestres para me graduar. Durante meu ensino médio realizei alguns cursos como eletrônica básica, robótica, Python básico, SQL básico e estudei inglês de forma autodidata.<br>
         </p>
     </div>
     <div class="habilidades">
+        <hr class="solid">
         <h1>Habilidades</h1>
+        <hr class="solid">
         <p>
             Uma das diretrizes da DB1 Group é "Feedback rápido e constante. (Realizar acompanhamentos com seu gestor semanalmente)", e durante minha atuação, nenhuma cerimônia de acompanhamento não foi realizada. Após 1 ano empregatício, é realizada uma cerimônia chamada ADM, e os pontos levantados pela minha chefe e companheiros foram:
         </p>
@@ -49,18 +57,22 @@
         </ul>
     </div>
     <div class="projetos">
+        <hr class="solid">
         <h1>Projetos</h1>
+        <hr class="solid">
         <p>
             No curso de robótica que realizei durante 2 anos, construí um robô para competir na modalidade sumô, onde deve-se derrubar o robô adversário em um buraco na arena.<br>
             Para que eu fosse direcionado para a área de desenvolvimento na DB1, fui designado para construir do zero um site intranet para os colaboradores do Anymarket. Desenvolvi esse site através do framework Wordpress e assim que ele foi lançado em produção, virou modelo para os outros sites intranet da empresa.
         </p>
     </div>
     <div class="contato">
+        <hr class="solid">
         <h1>Contato</h1>
+        <hr class="solid">
         <div class="social">
             <a href="https://www.linkedin.com/in/pedro-campana-de-souza" target="_blank"><img src="icons8-linkedin-48.png" alt="">LinkedIn</a>
-            <a href="https://www.instagram.com/pedro_cpsz" target="_blank"><img src="icons8-instagram-48.png" alt="">Instagram</a>
-            <a href="https://www.facebook.com/pedro.campanadesouza" target="_blank"><img src="icons8-facebook-48.png" alt="">Facebook</a>
+            <a href="https://www.instagram.com/pedro.cpnsz" target="_blank"><img src="icons8-instagram-48.png" alt="">Instagram</a>
+            <a href="https://www.facebook.com/pedro.cpnsz" target="_blank"><img src="icons8-facebook-48.png" alt="">Facebook</a>
         </div>
         <div class="formulario">
             <h2>Formulário para contato:</h2>
@@ -70,7 +82,7 @@
                 <label class="email">E-mail:</label>
                 <input type="email" name="email"><br>
                 <label class="tel">Telefone:</label>
-                <input type="tel" name="telefone" placeholder="(99) 99999-9999" data-mask="(00) 0000-0000"><br>
+                <input type="tel" name="telefone" placeholder="(99) 99999-9999" maxlength="11"><br>
                 <input class="enviar" type="submit" value="ENVIAR">
             </form>
         </div>
